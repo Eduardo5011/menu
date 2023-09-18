d6692ccf30358604bb1ac24fbf1725070f9a53f6
Eduardo5011 <eduardo.rosado5011@gmail.com>
2023-09-18 11:56:22 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Title from "./Title";
-
-
+import menu from "./data";
+import Menu from "./Menu";
 
 const App = () => {
-  return <main>
-    <section className="menu">
-  <Title/>
-    </section>
-  </main>;
+  const [menuItems, setMenuItems] = useState(menu);
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Menu items={menuItems}/>
+      </section>
+    </main>
+  );
 };
 export default App;
